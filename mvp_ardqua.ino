b7626353458e952c83eb6baa08e1e67d85adde93
KasisEarth <111049404+KasisEarth@users.noreply.github.com>
2025-12-09 10:05:15 +0100

    Button Konfiguration statt Kippschalter. LED Anzeige muss noch eingefügt werden

--- mvp_ardqua.ino
+++ mvp_ardqua.ino
@@ -1,7 +1,8 @@
 // --- Konfiguration ---
 const int PIN_SOIL = A0;   // Analog-Pin für Feuchtigkeitssensor
-const int PIN_SWITCH = A1; // 3-Stufen-Drehschalter
+const int PIN_SWITCH = A1; // Schalter
 const int PIN_PUMP = 8;    // Digital-Pin für Relais/MOSFET
+const int PIN_BUT = 9;     // Button fuer Modus-Umschaltung
 
 // --- Profil-Schwellwerte ---
 // Bereich: 0..1023, abhängig von Sensor-Kalibrierung
@@ -14,9 +15,15 @@ const unsigned long PUMP_WET_MS = 1000;    // 1s
 const unsigned long PUMP_NORMAL_MS = 2000; // 2s
 const unsigned long PUMP_DRY_MS = 3000;    // 3s
 
+const int thresholds[3] = {430, 520, 610};
+const unsigned long pumpTimes[3] = {1000, 2000, 3000};
+
+// Für geglättete Messung
+const int N_SAMPLES = 10;
+
 // TODO: aktuell kurze Intervalle zum Testen
 const unsigned long SAMPLE_INTERVAL_MS = 30000; // 30 Sekunden
-const unsigned long DEFAULT_PUMP_TIME = 1000; // nur Fallback
+const unsigned long DEFAULT_PUMP_TIME = 1000;   // nur Fallback
 
 // Optional: Hysterese
 const int HYSTERESIS = 20;
@@ -24,8 +31,23 @@ const int HYSTERESIS = 20;
 // --- Zustandsvariablen ---
 unsigned long lastSampleTs = 0;
 
-// Für geglättete Messung
-const int N_SAMPLES = 10;
+// Globale Variable, damit sie in Funktionen verändert werden kann
+int pumpMode = 0;
+
+void changePumpMode()
+{
+  if (pumpMode == 2)
+  {
+    pumpMode = 0;
+  }
+  else
+  {
+    pumpMode++;
+  }
+  Serial.println(F("Pump Modus: "));
+  Serial.println(pumpMode);
+  delay(1000);
+}
 
 int readSoilAveraged()
 {
@@ -36,19 +58,6 @@ int readSoilAveraged()
     delay(5);
   }
   return (int)(sum / N_SAMPLES);
-}
-
-// --- NEU: Funktion zur Auswertung des 3-Stufen-Schalters ---
-int getWaterProfile()
-{
-  int val = analogRead(PIN_SWITCH);
-
-  if (val < 341)
-    return 1; // feuchtigkeitsliebend
-  else if (val < 682)
-    return 2; // normal
-  else
-    return 3; // trocken
 }
 
 void runPump(const unsigned long runTime, const int threshold)
@@ -77,6 +86,7 @@ void setup()
 {
   pinMode(PIN_PUMP, OUTPUT);
   digitalWrite(PIN_PUMP, LOW);
+  pinMode(PIN_BUT, INPUT);
 
   Serial.begin(9600);
   Serial.println(F("Start: Bodenfeuchte-Autobewaesserung + Profilwahlschalter"));
@@ -84,9 +94,20 @@ void setup()
 
 void loop()
 {
+  int threshold = thresholds[0];
+  unsigned long pumpTime = pumpTimes[0];
+
+  if (digitalRead(PIN_BUT) == HIGH)
+  {
+    // Bei Knopfdruck wird der Modus eins hochgestellt
+    changePumpMode();
+    threshold = thresholds[pumpMode];
+    pumpTime = pumpTimes[pumpMode];
+  }
+
   unsigned long now = millis();
 
-  // Wenn millis von vorne beginnt, muss letzter Test reseted werden
+  // Wenn millis von vorne beginnt, muss letzter Samplezeitpunkt reseted werden
   if (now < lastSampleTs)
   {
     lastSampleTs = now;
@@ -97,35 +118,11 @@ void loop()
   {
     lastSampleTs = now;
 
-    int profile = getWaterProfile();
     int moisture = readSoilAveraged();
-
-    // --- Profilabhängige Parameter setzen ---
-    int threshold;
-    unsigned long pumpTime;
-
-    switch (profile)
-    {
-    case 1:
-      threshold = THRESH_WET;
-      pumpTime = PUMP_WET_MS;
-      break;
-    case 2:
-      threshold = THRESH_NORMAL;
-      pumpTime = PUMP_NORMAL_MS;
-      break;
-    case 3:
-      threshold = THRESH_DRY;
-      pumpTime = PUMP_DRY_MS;
-      break;
-    default:
-      threshold = THRESH_NORMAL;
-      pumpTime = DEFAULT_PUMP_TIME;
-    }
 
     // Debug-Ausgabe
     Serial.print(F("Profil: "));
-    Serial.print(profile);
+    Serial.print(pumpMode);
     Serial.print(F(" | Feuchtigkeit: "));
     Serial.print(moisture);
     Serial.print(F(" | Schwelle: "));
